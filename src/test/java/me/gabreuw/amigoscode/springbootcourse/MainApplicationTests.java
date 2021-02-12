11958304dbd5280517bd321aec784a209de1e917
ImGabreuw <imgabreuw.dev@gmail.com>
2021-02-12 16:33:57 -0300

    Projeto criado

---
+++ src/test/java/me/gabreuw/amigoscode/springbootcourse/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabreuw.amigoscode.springbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
