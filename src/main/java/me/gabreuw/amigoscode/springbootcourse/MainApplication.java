7269f1be16727b6edcefc423fb33747cfde4a261
ImGabreuw <imgabreuw.dev@gmail.com>
2021-02-13 16:24:50 -0300

    API simples com Spring Boot

--- src/main/java/me/gabreuw/amigoscode/springbootcourse/MainApplication.java
+++ src/main/java/me/gabreuw/amigoscode/springbootcourse/MainApplication.java
@@ -2,12 +2,25 @@ package me.gabreuw.amigoscode.springbootcourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class MainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello() {
+		return List.of(
+				"Hello",
+				" World"
+		);
 	}
 
 }
