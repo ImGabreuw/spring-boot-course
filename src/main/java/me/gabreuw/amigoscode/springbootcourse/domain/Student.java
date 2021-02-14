aa01ab1aa39b06e43f3e1e9bbc84feb4185a7623
ImGabreuw <imgabreuw.dev@gmail.com>
2021-02-14 17:22:11 -0300

    JPA e @Entity

--- src/main/java/me/gabreuw/amigoscode/springbootcourse/domain/Student.java
+++ src/main/java/me/gabreuw/amigoscode/springbootcourse/domain/Student.java
@@ -4,14 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "student")
 public class Student implements Serializable {
 
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
 
     private String name;
